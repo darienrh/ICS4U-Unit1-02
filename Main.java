267c591b79f7e2fb53555b497eb73ff9a31a6a70
darienrh <94391400+darienrh@users.noreply.github.com>
2021-12-15 23:07:33 +0000

    idk

--- Main.java
+++ Main.java
@@ -38,17 +38,17 @@ final class Main {
     public static void main(final String[] args) {
         double mass = 0.0;
         double energy = 0.0;
-        Scanner myInput= new Scanner (System.in);  //define instance of Scanner
+        Scanner myInput = new Scanner(System.in);  //define instance of Scanner
         System.out.print("Enter the mass of the object (kg) : ");
         try {
             mass = myInput.nextDouble(); //get input from the user
             energy = mass * SPEEDOFLIGHT * SPEEDOFLIGHT;  //do the math
-            System.out.println("An object of mass " + mass + "would produce "+ energy + " J of Energy");  //Print the result
-        }
-        catch (Exception e) {
+            System.out.println("An object of mass " + mass + "would produce "
+              + energy + " J of Energy");
+        } catch (java.util.InputMismatchException errorCode) {
             System.out.println("Sorry that was not a number !!!");
         }
-    
+
       System.out.println("\nDone.");
     }
 }
